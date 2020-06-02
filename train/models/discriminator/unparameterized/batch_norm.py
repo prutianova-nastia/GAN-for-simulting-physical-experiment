aa6ccb466e79dc84d6cbf063eeaf9fc959d8e04a
prutianova-nastia <auprutyanova@gmail.com>
2020-06-02 15:22:16 +0300

    batch norm unparametrized discriminator

--- train/models/discriminator/unparameterized/batch_norm.py
+++ train/models/discriminator/unparameterized/batch_norm.py
@@ -3,27 +3,27 @@ import tensorflow as tf
 
 def make_discriminator():
     return tf.keras.Sequential([
-        tf.keras.layers.Conv2D(20, 3, input_shape=(8, 8, 1)),
+        tf.keras.layers.Conv2D(16, 3, input_shape=(8, 8, 1)),
         tf.keras.layers.LeakyReLU(),
         tf.keras.layers.Dropout(0.3),
 
-        tf.keras.layers.Conv2D(20, 3, padding='same'),
+        tf.keras.layers.Conv2D(16, 3, padding='same'),
         tf.keras.layers.LeakyReLU(),
         tf.keras.layers.Dropout(0.3),
 
-        tf.keras.layers.Conv2D(40, 3),
+        tf.keras.layers.Conv2D(32, 3),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.LeakyReLU(),
 
         tf.keras.layers.Dropout(0.2),
 
-        tf.keras.layers.Conv2D(40, 3, padding='same'),
+        tf.keras.layers.Conv2D(32, 3, padding='same'),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.LeakyReLU(),
 
         tf.keras.layers.Dropout(0.3),
 
-        tf.keras.layers.Conv2D(80, 3),
+        tf.keras.layers.Conv2D(64, 3),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.LeakyReLU(),
 
